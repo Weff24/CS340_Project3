d649d0e0b53bbe72104f9e5a9cf39bb76579daab
Weff24 <jwny123@gmail.com>
2024-02-25 16:58:49 -0600

    Cleaned up code and added comments

--- distance_vector_node.py
+++ distance_vector_node.py
@@ -14,39 +14,22 @@ class Distance_Vector_Node(Node):
 
     # Return a string
     def __str__(self):
-        return "Rewrite this function to define your node dump printout"
+        return "Node " + str(self.id)
 
     # Fill in this function
     def link_has_been_updated(self, neighbor, latency):
         # latency = -1 if delete a link
         if latency == -1 and neighbor in self.neighbors:
             self.neighbors.remove(neighbor)
-
-            # if neighbor in self.neighboring_dvs:
             del self.neighboring_dvs[neighbor]
             del self.neighboring_seq_nums[neighbor]
             del self.links[neighbor]
                 
         else: 
             self.neighbors.append(neighbor)
-        
             self.links[neighbor] = latency
-
-
-        # prev_dvs = copy.deepcopy(self.dvs)
-        # seq_num = 0
-        # if neighbor in self.dvs.keys():
-        #     seq_num = self.dvs[neighbor]["seq_num"] + 1
-
-        # self.dvs[neighbor] = {
-        #     # "seq_num": seq_num,
-        #     "cost": latency,
-        #     "path": [neighbor]
-        # }
-
-        # for (n, neighbor_dv) in self.neighboring_dvs.items():
-        #     self.update_dv(n, neighbor_dv)
             
+        # Update node's distance vector links
         self.dvs = {}
         for (n, cost) in self.links.items():
             self.dvs[n] = {
@@ -54,20 +37,22 @@ class Distance_Vector_Node(Node):
                 "path": [self.id, n]
             }
 
+        # Update distance vectors based on neighbors and send updated distance vector to neighbors
         self.update_dv()
         self.send_to_neighbors(json.dumps(self.get_message()))
 
-        
+    #  Update distance vectors of current node based on known neighboring distance vectors
     def update_dv(self):
+        # Update node sequence number
         self.seq_num += 1
-        # print(self.neighboring_dvs)
-        # print(self.links)
         for (n, neighbor_dv) in self.neighboring_dvs.items():
             if n in self.links:
                 for (destination, val) in neighbor_dv.items():
+                    # Check for potential loops or removed links
                     if destination == self.id or self.id in val["path"] or val["cost"] <= 0:
                         continue
 
+                    # Update distance vector based on cost
                     if (destination not in self.dvs.keys()) or ((destination in self.dvs.keys()) and (self.links[n] + val["cost"] < self.dvs[destination]["cost"])):
                         new_path = copy.deepcopy(val["path"])
                         new_path.insert(0, self.id)
@@ -76,7 +61,7 @@ class Distance_Vector_Node(Node):
                             "path": new_path
                         }
 
-
+    # Get the json of the message that is sent to neighbors
     def get_message(self):
         message = {
             self.id: {},
@@ -88,13 +73,15 @@ class Distance_Vector_Node(Node):
 
     # Fill in this function
     def process_incoming_routing_message(self, m):
+        # Make a copy of node's distance vectors to check for changes
         prev_dvs = copy.deepcopy(self.dvs)
+
+        # Load message
         neighboring_dv = json.loads(m)
         neighbor = int(list(neighboring_dv.keys())[0])
         seq_num = neighboring_dv["seq_num"]
 
-        # change_occurred = False
-
+        # Check if this message is new
         if (neighbor not in self.neighboring_dvs.keys()) or (seq_num > self.neighboring_seq_nums[neighbor]):
             parsed_neighboring_dvs = {
                 int(key):val for (key, val) in neighboring_dv[str(neighbor)].items()
@@ -102,33 +89,22 @@ class Distance_Vector_Node(Node):
             self.neighboring_dvs[neighbor] = parsed_neighboring_dvs
             self.neighboring_seq_nums[neighbor] = seq_num
 
-            # self.links[neighbor] = parsed_neighboring_dvs[self.id]
-            # print(parsed_neighboring_dvs)
-
+            # Update node's distance vector
             self.dvs = {}
             for (n, cost) in self.links.items():
                 self.dvs[n] = {
                     "cost": cost,
                     "path": [self.id, n]
                 }
-                
-            # for dest in parsed_neighboring_dvs.keys():
-            #     if dest in self.dvs and neighbor in self.dvs[dest]["path"]:
-            #         del self.dvs[dest]
-            
             self.update_dv()
-            # print("HIHIHIHIH")
-            # print(neighbor)
-            # print(self.id)
-            # print(self.dvs)
             
+            # Send distance vector to neighbors if there was a change
             if self.dvs != prev_dvs:
                 self.send_to_neighbors(json.dumps(self.get_message()))
 
     # Return a neighbor, -1 if no path to destination
     def get_next_hop(self, destination):
-        # print("DVSSSSS")
-        # print(self.dvs)
+        # Get the next node in the path
         if destination in self.dvs.keys():
             return self.dvs[destination]["path"][1]
         return -1
--- link_state_node.py
+++ link_state_node.py
@@ -9,7 +9,7 @@ class Link_State_Node(Node):
 
     # Return a string
     def __str__(self):
-        return "Rewrite this function to define your node dump printout"
+        return "Node " + str(self.id)
 
     # Fill in this function
     def link_has_been_updated(self, neighbor, latency):
@@ -18,16 +18,14 @@ class Link_State_Node(Node):
             self.neighbors.remove(neighbor)
         else: 
             self.neighbors.append(neighbor)
-            # self.send_to_neighbors("hello")
-            # self.send_to_neighbor(neighbor, "hello")
         
+        # Get sequence number of link
         seq_num = 0
         if frozenset((self.id, neighbor)) in self.links.keys():
             seq_num = self.links[frozenset((self.id, neighbor))]["seq_num"] + 1
 
+        # Add or update link
         self.links[frozenset((self.id, neighbor))] = {
-            # "src": id,
-            # "dest": neighbor,
             "seq_num": seq_num,
             "cost": latency
         }
@@ -36,24 +34,24 @@ class Link_State_Node(Node):
 
         self.logging.debug('link update, neighbor %d, latency %d, time %d' % (neighbor, latency, self.get_time()))
 
+    # Get the json of the message that is sent to neighbors
     def get_message(self):
         message = {}
         for (key, val) in self.links.items():
-            # if key[0] == self.id:
-            #     message[str((key[1], key[0]))] = val
-            # else:
-            #     message[str(key)] = val
             message[str(tuple(key))[1:-1]] = val
         return message
 
     # Fill in this function
     def process_incoming_routing_message(self, m):
+        # Load message
         message_links = json.loads(m)
         change_occurred = False
 
+        # Update links
         for (key, val) in message_links.items():
             parsed_key = frozenset(tuple(map(int, key.split(', '))))
             if parsed_key in self.links.keys():
+                # Check if link is new
                 if val["seq_num"] > self.links[parsed_key]["seq_num"]:
                     self.links[parsed_key] = val
                     change_occurred = True
@@ -61,28 +59,33 @@ class Link_State_Node(Node):
                 self.links[parsed_key] = val
                 change_occurred = True
 
-        
+        # Send update to neighbors if links changed
         if change_occurred:
             self.send_to_neighbors(json.dumps(self.get_message()))
 
     # Return a neighbor, -1 if no path to destination
     def get_next_hop(self, destination):
-        # distances maps from node id to cost to get the specific node
+        # Map from node id to cost to get the specific node
         distances = {
             self.id: 0
         }
+
+        # Queue of nodes to check
         queue = [{
             "id": self.id,
             "cost": 0,
             "next": self.id 
         }]
 
+        # Check potential nodes
         while queue:
             node = queue.pop(0)
 
+            # Reached destination node
             if node["id"] == destination:
                 return node["next"]
 
+            # Check if there are any shorter paths (less cost)
             for (key, val) in self.links.items():
                 if (val["cost"] > 0) and (node["id"] in key):
                     neighbor = tuple(key)[1] if tuple(key)[0] == node["id"] else tuple(key)[0]
@@ -95,6 +98,8 @@ class Link_State_Node(Node):
                             "cost": new_cost,
                             "next": neighbor if self.id == node["id"] else node["next"]
                         })
+
+            # Sort queue to get next min cost value
             queue.sort(key=lambda x: x["cost"])
 
         return -1
